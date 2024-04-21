da1972d514f0d0b02e7097ca9a2597bea8055760
oatItsaraphong <oat.itsaraphong@gmail.com>
2024-04-21 12:44:41 -0700

    update question card

--- questcraft/app/questions/page.tsx
+++ questcraft/app/questions/page.tsx
@@ -2,13 +2,22 @@
 import React from 'react';
 import './App.css';
 import Questionnaire from './question_one';
+import styles from '../styles/question.module.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Questionnaire</h1>
+      <link href='https://fonts.googleapis.com/css?family=Londrina Solid' rel='stylesheet'/>
+        <div className={styles.question_card}
+        style={{
+            backgroundImage: 'url("/images/border_question.png")',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+        }}
+        >
         <Questionnaire />
+        </div>
       </header>
     </div>
   );
--- questcraft/app/questions/question_one.tsx
+++ questcraft/app/questions/question_one.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import styles from './question.css';
+import styles from '../styles/question.module.css';
 import { useRouter } from 'next/navigation';
 
 
@@ -10,34 +10,34 @@ const questions = [
         id: 1, 
         text: "Would you rather live in a friendly village or a secret cave in the forest?",
         options: ["House in a village", "Cave in a forest"]
+    },
+
+    { 
+        id: 2, 
+        text: "Would you rather wear fancy clothes or simple everyday clothes?",
+        options: ["Fancy clothes", "Simple clothes"]
+    },
+    { 
+        id: 3, 
+        text: "How would you like to defeat bad guys?",
+        options: ["My cool magic", "My super strength"]
+    },
+    {
+        id: 4, 
+        text: "Would you rather support your friends or lead the way?",
+        options: ["Support", "Lead"]
+    },
+    {
+        id: 5, 
+        text: "Would you rather be known for your bravery and honor or your cleverness and knowledge?",
+        options: ["Bravery and honor", "Cleverness and knowledge"]
+    },
+    {
+        id: 6, 
+        text: "Do you want to work as a lone wolf or as part of a team?",
+        options: ["Lone wolf", "Part of a team"]
     }
 ]
-//     { 
-//         id: 2, 
-//         text: "Would you rather wear fancy clothes or simple everyday clothes?",
-//         options: ["Fancy clothes", "Simple clothes"]
-//     },
-//     { 
-//         id: 3, 
-//         text: "How would you like to defeat bad guys?",
-//         options: ["My cool magic", "My super strength"]
-//     },
-//     {
-//         id: 4, 
-//         text: "Would you rather support your friends or lead the way?",
-//         options: ["Support", "Lead"]
-//     },
-//     {
-//         id: 5, 
-//         text: "Would you rather be known for your bravery and honor or your cleverness and knowledge?",
-//         options: ["Bravery and honor", "Cleverness and knowledge"]
-//     },
-//     {
-//         id: 6, 
-//         text: "Do you want to work as a lone wolf or as part of a team?",
-//         options: ["Lone wolf", "Part of a team"]
-//     }
-// ]
 
 function createQuery(answerlist: { [x: string]: string; }){
     console.log("abbb")
@@ -61,10 +61,12 @@ function createQuery(answerlist: { [x: string]: string; }){
 function Questionnaire() {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [answers, setAnswers] = useState({});
+    const [selected, setSelected] = useState("");
     const router = useRouter();
     
   
     const handleOptionChange = (questionId: number, option: string) => {
+      setSelected(option);
       setAnswers(prevAnswers => ({
         ...prevAnswers,
         [questionId]: option
@@ -87,36 +89,82 @@ function Questionnaire() {
       };
     const currentQuestion = questions[currentQuestionIndex];
     const isLastQuestion = currentQuestionIndex === questions.length - 1;
+
+
+
+
   
     return (
-      <div>
-        <h1>Questionnaire</h1>
-        <p>{currentQuestion.text}</p>
-        {currentQuestion.options.map(option => (
-          <div key={option}>
-            <input
-              type="radio"
-              id={option}
-              name={currentQuestion.id}
-              value={option}
-              checked={answers[currentQuestion.id] === option}
-              onChange={() => handleOptionChange(currentQuestion.id, option)}
-            />
-            <label htmlFor={option}>{option}</label>
-          </div>
-        ))}
+      <div className={styles.card_container}>
+        <h1 className={styles.q_title}>Character Builder</h1>
+        <p className={styles.q_question}>{currentQuestion.text}</p>
+        
+        <div className={styles.q_option_left}>
+        <label key={currentQuestion.options[0]} className={selected === currentQuestion.options[0] ? styles.card_selected: styles.card}>
+                <input
+                  type="radio"
+                  id={currentQuestion.options[0]}
+                  name={currentQuestion.text}
+                  value={currentQuestion.options[0]}
+                  checked={selected === currentQuestion.options[0]}
+                  onChange={() => handleOptionChange(currentQuestion.id, currentQuestion.options[0])}
+                  className={styles.card_input_element}
+                />
+                <div className={styles.card_content}>
+                  <h4>{currentQuestion.options[0]}</h4>
+                </div>
+            </label>
+            </div>
+
+
+        <div className={styles.q_option_right}>
+            <label key={currentQuestion.options[1]} className={selected === currentQuestion.options[1] ? styles.card_selected: styles.card}>
+              <input
+                type="radio"
+                id={currentQuestion.options[1]}
+                name={currentQuestion.text}
+                value={currentQuestion.options[1]}
+                checked={selected === currentQuestion.options[1]}
+                onChange={() => handleOptionChange(currentQuestion.id, currentQuestion.options[1])}
+                className={styles.card_input_element}
+              />
+              <div className={styles.card_content}>
+                <h4>{currentQuestion.options[1]}</h4>
+              </div>
+            </label>
+            </div>
+
+        {/* {currentQuestion.options.map(option => (
+            <label key={option} className={selected === option ? styles.card_selected: styles.card}>
+
+                <input
+                  type="radio"
+                  id={option}
+                  name={currentQuestion.text}
+                  value={option}
+                  checked={selected === option}
+                  onChange={() => handleOptionChange(currentQuestion.id, option)}
+                  className={styles.card_input_element}
+                />
+                <div className={styles.card_content}>
+                  <h4>{option}</h4>
+                </div>
+            </label>
+        ))} */}
+
         <button 
           disabled={currentQuestionIndex === 0}
           onClick={() => navigateQuestions(-1)}
+          className={styles.q_nav_pre}
         >
-          Previous
+          &laquo; Previous
         </button>
 
         {isLastQuestion ? (
-            <button type="submit" onClick={handleSubmit}>Submit</button>
+            <button className={styles.q_nav_next} type="submit" onClick={handleSubmit}>Submit</button>
         ) : (
-            <button type="button" onClick={handleNext}>
-            Next
+            <button className={styles.q_nav_next} type="button" onClick={handleNext}>
+            Next &raquo;
             </button>
         )}
       </div>
