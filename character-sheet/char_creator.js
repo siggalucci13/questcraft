585b6270a3bf79ae9cbe51924d93f6b2620aaf8c
konaspy <christian.shin@gmail.com>
2024-04-20 19:32:16 -0700

    Update char_creator.js

--- character-sheet/char_creator.js
+++ character-sheet/char_creator.js
@@ -17,27 +17,27 @@ template = {
     "equipment": []
 }
 
-stat_names = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+statNames = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
 
-class_stats = {
+classStats = {
     "Warrior": [15, 13, 14, 8, 12, 10],
     "Rogue": [12, 15, 14, 8, 13, 10],
     "Wizard": [8, 13, 14, 15, 12, 10],
     "Bard": [8, 13, 14, 10, 12, 15]}
 
-race_bonus = {
+raceBonus = {
     "Human": [1, 1, 1, 1, 1, 1],
     "Elf": [0, 2, 0, 1, 0, 0],
     "Dragonborn": [2, 0, 0, 0, 0, 1],
     "Orc": [2, 0, 1, 0, 0, 0]}
 
-spell_list = {
+spellList = {
     "Warrior": {},
     "Rogue": {},
     "Wizard": {"Mage Hand": ["Cantrip", "tba"], "Fire Bolt": ["Cantrip", "tba"], "Magic Missle": [2, "[tba]"]},
     "Bard": {"Prestidigitation": ["Cantrip", "tba"], "Vicious Mockery": ["Cantrip", "tba"],  "Healing Word": [2, "tba"]}}
 
-skill_list = {
+skillList = {
     "Warrior": {"Defense": "Add 1 to your Armor Class (AC)"},
     "Rogue": {"Sneak Attack": "Deal an extra 1d6 damage if an ally is next to the target."},
     "Wizard": {"Arcane Recovery": "Regain your spells after a short rest."},
@@ -51,7 +51,7 @@ equipment = {
     "Bard": ["Rapier", "Harp", "Leather Armor"]
 }
 
-armor_class = {
+armorClass = {
     "Warrior": 19,
     "Rogue": 14,
     "Wizard": 12,
@@ -70,25 +70,7 @@ function createSheet(characterName, className, race, background) {
     sheet["armor_class"] = armorClass[className];
     for (let i = 0; i < 6; i++) {
         let statName = statNames[i];
-        sheet[statName] = Math.floor((class_stats[className][i] + race_bonus[className][i] - 10) / 2);
+        sheet[statName] = Math.floor((classStats[className][i]+raceBonus[race][i]-10)/2);
     }
     return sheet;
 }
-
-// # def create_sheet(character_name: str, class_name: str, race: str, background: str):
-// #     sheet = dict(template)
-// #     sheet["character_name"] = character_name
-// #     sheet["class"] = class_name
-// #     sheet["background"] = background
-// #     sheet["race"] = race
-// #     sheet["spell_list"] = spell_list[class_name]
-// #     sheet["skill_list"] = skill_list[class_name]
-// #     sheet["equipment"] = equipment[class_name]
-// #     sheet["armor_class"] = armor_class[class_name]
-// #     character_stats = [(x + y - 10) // 2 for x, y in zip(class_stats[class_name], race_bonus[race])]
-// #     for i in range(6):
-// #         stat_name = stat_names[i]
-// #         sheet[stat_name] = character_stats[i]
-// #     return sheet
-
-console.log(create_sheet("christian", "Bard", "Dragonborn", "Hero"))
