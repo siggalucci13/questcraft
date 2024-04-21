eb579f77a5a5c82ec9d0eb81f839736fd6e4ad99
konaspy <christian.shin@gmail.com>
2024-04-20 20:15:36 -0700

    Create questions.js

---
+++ character-sheet/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    { 
+        id: 1, 
+        text: "Would you rather live in a friendly village or a secret cave in the forest?",
+        options: ["House in a village", "Cave in a forest"]
+    },
+    { 
+        id: 2, 
+        text: "Would you rather wear fancy clothes or simple everyday clothes?",
+        options: ["Fancy clothes", "Simple clothes"]
+    },
+    { 
+        id: 3, 
+        text: "How would you like to defeat bad guys?",
+        options: ["My cool magic", "My super strength"]
+    },
+    {
+        id: 4, 
+        text: "Would you rather support your friends or lead the way?",
+        options: ["Support", "Lead"]
+    },
+    {
+        id: 5, 
+        text: "Would you rather be known for your bravery and honor or your cleverness and knowledge?",
+        options: ["Bravery and honor", "Cleverness and knowledge"]
+    },
+    {
+        id: 6, 
+        text: "Do you want to work as a lone wolf or as part of a team?",
+        options: ["Lone wolf", "Part of a team"]
+    }
+]
