5b8703092c566b50bc08bd9647e70ddae19a4a42
oatItsaraphong <oat.itsaraphong@gmail.com>
2024-04-21 13:33:54 -0700

    update look

--- questcraft/app/page.tsx
+++ questcraft/app/page.tsx
@@ -6,37 +6,35 @@ import styles from '../app/styles/Page.module.css';
 
 const Page: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container}
+    style={
+      {
+        backgroundImage: 'url("/images/questcraft.png")',
+
+        backgroundPosition: 'center'
+        
+      }
+    }>
       <Head>
+
+        <link href='https://fonts.googleapis.com/css?family=Londrina Solid' rel='stylesheet'/>
+      
         <title>QuestCraft</title>
         <meta name="description" content="Embark on an exciting adventure game!" />
         <link rel="icon" href="/favicon.ico" />
+        
       </Head>
 
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to QuestCraft!</h1>
+        {/* <h1 className={styles.title}>Welcome to QuestCraft!</h1>
         <p className={styles.description}>
           Get ready to embark on an exciting journey filled with adventure and imagination!
-        </p>
+        </p> */}
         <div className={styles.button}>
-          <Link href="/character-creation">
-            <button>Start Character Creation</button>
+          <Link href="/questions">
+            <button className={styles.next_button}>Start Character Creation</button>
           </Link>
         </div>
       </main>
